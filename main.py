743ae7da4c54eed6272d4e1b078205d98fb97b9c
Camden Hurd <112513088+camdenHurdStBonas@users.noreply.github.com>
2024-09-17 14:15:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import uuid
-from trading_strategy import trading_strategy  # Import the function
-from robinhood_api_trading import CryptoAPITrading  # Assuming you have this class defined in CryptoAPITrading.py
+# main.py
+from scheduler import start_scheduler
+from robinhood_api_trading import CryptoAPITrading
+from trading_strategy import trading_strategy
 
 def main():
     api_trading_client = CryptoAPITrading()
@@ -9,8 +10,14 @@ def main():
     print("Account details:")
     print(api_trading_client.get_account())
 
-    # Run the trading strategy
-    trading_strategy(api_trading_client)
+    # Start the scheduler with the trading strategy function and desired interval
+    start_scheduler(trading_strategy, 1)  # Run trading strategy every 10 seconds
+    
+    # Fetch and print account details
+    print("Account details:")
+    print(api_trading_client.get_account())
+    
+    print("Done!")
 
 if __name__ == "__main__":
     main()
