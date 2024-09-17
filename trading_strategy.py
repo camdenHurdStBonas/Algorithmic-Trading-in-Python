508dcbfc8d825b53146493464c925af1caaf227d
Camden Hurd <112513088+camdenHurdStBonas@users.noreply.github.com>
2024-09-17 13:26:52 -0400

    Create trading_strategy.py

---
+++ trading_strategy.py
@@ -0,0 +1,15 @@
+from typing import Any
+import uuid
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+def trading_strategy(api_trading_client: Any):
+  
+    # Get Best Bid/Ask Prices
+    print("\nFetching Best Bid/Ask for BTC-USD...")
+    best_bid_ask = api_trading_client.get_best_bid_ask("BTC-USD")
+    print("Best Bid/Ask response:")
+    print(best_bid_ask)
+    
